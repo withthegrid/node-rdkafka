78e17cd642a13089d1b390cdd3e4f5b4f5682200
Martijn Imhoff <info@martijnimhoff.nl>
2024-01-10 10:19:39 +0100

    Remove hardcoded partitions

--- test-consume.js
+++ test-consume.js
@@ -2,7 +2,7 @@ var KafkaNode = require('./lib/index.js');
 
 var consumer = new KafkaNode.KafkaConsumer(
   {
-    'group.id': 'testing-44',
+    'group.id': 'testing-54',
     'metadata.broker.list': 'localhost:9093',
     'enable.auto.commit': false,
   },
@@ -11,21 +11,54 @@ var consumer = new KafkaNode.KafkaConsumer(
   }
 );
 
+consumer.on('subscribed', function(topics) {
+  console.log('subscribed: ', topics, ' assignments: ', consumer.assignments());
+});
+
 consumer.connect();
 
 var topicName = 'consume-per-partition';
+var assignments = [];
 
 consumer.on('ready', function () {
   console.log('ready');
 
   consumer.subscribe([topicName]);
 
-  consumeMessages(0, 100);
-  consumeMessages(1, 1000);
+  // check assigned partitions
+  // and start consume for newly assigned partitions
+  setInterval(updateAssignmentsAndStartConsumeLoops, 1000);
 });
 
-function consumeMessages(partition, timeout) {
+function updateAssignmentsAndStartConsumeLoops() {
+  var updatedAssignments = consumer.assignments();
+
+  // find new assignments
+  var newAssignments = updatedAssignments.filter(function (a) {
+    return !assignments.includes(a);
+  });
+
+  newAssignments.forEach(function (assignment) {
+    startConsumeMessages(assignment.partition);
+  });
+
+  assignments = updatedAssignments;
+}
+
+function startConsumeMessages(partition) {
+  // simulate different performance
+  var timeout = partition === 0 ? 500 : 2000;
+
   function consume() {
+    var isPartitionAssigned = assignments.includes(function(assignment) {
+      return assignment.partition === partition;
+    });
+
+    if (!isPartitionAssigned) {
+      // stop consuming
+      return;
+    }
+
     consumer.consume(10, topicName, partition, callback);
   }
 
@@ -35,10 +68,19 @@ function consumeMessages(partition, timeout) {
       consumer.commitMessage(message);
     });
 
-    // wait for the timeout and then trigger the next consume
+    // simulate performance
     setTimeout(consume, timeout);
   }
 
   // kick-off recursive consume loop
   consume();
 }
+
+
+process.on('SIGUSR2', function() {
+  console.log('process received SIGUSR2. Shutting down consumer');
+  consumer.disconnect(function () {
+    console.log('Consumer disconnected. Shutting down...');
+    process.exit();
+  });
+});
